efa1e95fa0ccb7688b9210b7bb00fb5daa410580
Hassanstar1 <hassan.loulou1@gmail.com>
2017-07-10 16:53:20 +0200

    Grammar v1

--- parse6.py
+++ parse6.py
@@ -2,13 +2,21 @@ from nltk import *
 
 grammar1 = CFG.fromstring("""
   S -> PrePolitesse V Contacts Det Intent Telling
+  S -> V Contacts Det Intent Telling PrePolitesse
+  S -> V DestinationP Contacts Det Intent Telling PrePolitesse
+  S -> V DestinationP Contacts Det Intent Telling
+  S -> V Det Intent DestinationP Contacts Telling PrePolitesse
+  S -> V Det Intent Contacts Telling PrePolitesse
+  S -> PrePolitesse V Det Intent Contacts Telling
+  S -> V Det Intent Contacts Telling
+  S -> V Det Intent DestinationP Contacts Telling 
   Telling -> "says:" | ",that_says:" | "that" "says" | "that" "tells" | "that" "tell" | "that" "informs" | "informing"
   PP -> DestinationP Contacts
   PrePolitesse -> "would" "you" "please" | "please" | "would" "you" | "would" "you" "like" | "could" "you" | "I wish" | "I wish you" | "thanks" "to" | "thanks" "for" |
   V -> "send" | "text" | "message" | "sending" | "messaging" | "texting"
   Intent -> "sms" | "message"
   Det -> "a" | "an" | "the" | "my" | "your" | "that" |
-  DestinationP -> "for"| "at" | "to" | "into"
+  DestinationP -> "to" | "into"
   Contacts -> "Shadi" | "Ahmad" | "Ali" | "Samer" | "Hassan"
 """)
 
