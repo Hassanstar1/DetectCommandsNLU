35afcfedc4064f62449c8e39d1e8ebee4786a373
ALISem <baddour.ali85@gmail.com>
2017-07-27 18:53:07 +0800

    Update parse11.py
    This parser handles sentences that start with a verb or intent phrase and end with a content.

--- parse11.py
+++ parse11.py
@@ -5,117 +5,124 @@ from nltk.tree import *
 from nltk.draw import tree
 
 """@Samer Test """
-sentences =["send an sms to dad at 9 am repeat everyday content good morning dad say it loudly",
-            "send an sms to dad at 9 am content good morning dad say it loudly",
+sentences =["send to dad an sms at 9 am repeat everyday content take medication at 12 am go to meet in xx",
+            "send an sms to dad at 9 am repeat everyday say it loudly content good morning dad",
+            "send an sms to dad at 9 am say it loudly content good morning dad",
             "send an sms to dad at 9 am content good morning dad",
-            "send an sms to dad content good morning dad say it loudly",
-            "send an sms to dad at 9 am repeat everyday  content good morning dad say it loudly ",
-            "send to dad an sms at 9 am repeat everyday  content good morning dad say it loudly ",
-            "send to dad at 9 am repeat everyday  an sms content good morning dad say it loudly ",
-            "send to dad an sms at 9 am repeat everyday  content good morning dad say it loudly ",
-            "send an sms at 9 am to dad repeat everyday  content good morning dad say it loudly ",
-            "send an sms at 9 am repeat everyday  to dad content good morning dad say it loudly ",
-            "send an sms at 9 am repeat everyday content good morning dad to dad say it loudly ",
-            "send at 9 am an sms to dad repeat everyday  content good morning dad say it loudly",
-            "send an sms at 9 am to dad repeat everyday  content good morning dad say it loudly",
-            "send an sms to dad repeat everyday  at 9 am content good morning dad say it loudly",
-            "send an sms to dad repeat everyday  content good morning at 9 am dad say it loudly",
-            "send an sms to dad repeat everyday  content good morning  dad say it loudly at 9 am",
-            "send an sms repeat everyday to dad at 9 am  content good morning dad say it loudly",
-            "send an sms to dad repeat everyday  at 9 am  content good morning dad say it loudly",
-            "send an sms to dad at 9 am  content good morning dad repeat everyday  say it loudly",
-            "send an sms to dad at 9 am  content good morning dad say it loudly repeat everyday  ",
-            "send an sms content good morning dad to dad at 9 am repeat everyday  say it loudly",
-            "send an sms to dad content good morning dad at 9 am repeat everyday  say it loudly",
-            "send an sms to dad at 9 am content good morning dad repeat everyday  say it loudly",
-            "send an sms to dad at 9 am repeat everyday  say it loudly content good morning dad",
-            "send an sms say it loudly to dad at 9 am repeat everyday  content good morning dad ",
-            "send an sms to dad say it loudly at 9 am repeat everyday  content good morning dad ",
-            "send an sms to dad at 9 am say it loudly repeat everyday  content good morning dad ",
-            "send an sms to dad at 9 am repeat everyday  say it loudly content good morning dad "]
+            "send an sms to dad say it loudly content good morning dad",
+            "send an sms to dad content good morning dad",
+            "send an sms to dad say it loudly at 9 am repeat everyday content good morning dad",
+            "send to dad an sms at 9 am repeat everyday content good morning dad",
+            "send to dad at 9 am repeat everyday an sms say it loudly content good morning dad",
+            "send to dad an sms say it loudly at 9 am repeat everyday content good morning dad",
+            "send an sms at 9 am to dad repeat everyday content good morning dad",
+            "send an sms at 9 am to dad repeat everyday say it loudly content good morning dad",
+            "send an sms at 9 am repeat everyday to dad content good morning dad",
+            "send an sms at 9 am repeat everyday say it loudly to dad content good morning dad",
+            "send at 9 am an sms to dad repeat everyday content good morning dad",
+            "send at 9 am an sms to dad repeat everyday say it loudly content good morning dad",
+            "send an sms at 9 am to dad say it loudly repeat everyday content good morning dad",
+            "send an sms to dad repeat everyday at 9 am content good morning dad",
+            "send an sms to dad repeat everyday at 9 am say it loudly content good morning dad",
+            "send an sms to dad repeat everyday say it loudly at 9 am content good morning dad",
+            "send an sms to dad at 9 am say it loudly repeat everyday content good morning dad",
+            "send an sms repeat everyday to dad at 9 am content good morning dad",
+            "send an sms repeat everyday to dad at 9 am say it loudly content good morning dad",
+            "send an sms repeat everyday say it loudly to dad at 9 am content good morning dad",
+            "send an sms say it loudly repeat everyday to dad at 9 am content good morning dad",
+            "send an sms repeat everyday to dad say it loudly at 9 am content good morning dad",
+            "send an sms say it loudly to dad repeat everyday at 9 am content good morning dad",
+            "send an sms to dad say it loudly repeat everyday at 9 am content good morning dad",
+            "send an sms to dad at 9 am repeat everyday say it loudly content good morning dad",
+            "send an sms at 9 am repeat everyday to dad say it loudly content good morning dad",
+            "send an sms at 9 am say it loudly repeat everyday to dad content good morning dad",
+            "send an sms to dad at 9 am repeat everyday content good morning dad",
+            "send to dad an sms at 9 am repeat everyday say it loudly content good morning dad",
+            "send an sms say it loudly to dad at 9 am repeat everyday content good morning dad"]
 
-
-"""
-sentences =["tell dad to take his medication now say it loudly",
-            "send an sms to Hassan telling him that I can not come today and I am sorry but we can meet tomorrow",
-            "Ali please take your medicine",
-            "sorry Ahmad I can not visit you today",
-            "would you please send an sms to Samer and inform him that xxx xxx send this message tomorrow at 7 am",
-            "I wish to send an sms to Ahmad that says go home as soon as possible this message must be sent today evening at 7"]
-
-
-sentences = ["please send an sms daily at 2 pm to Ali body take your medication say it loudly",
-
-             "send an sms to dad at 3 pm content take your medication now say it loudly",
-             "tell dad to take your medication now say it loudly",
-             "please send an sms to Shadi body please check your email asap say it loudly",
-             "send Ahmad a message tells read your speech loudly say it loudly",
-             "text Ali at 8 pm tells it is a friendly reminder about our meeting today at 9 pm say it loudly",
-             "send Hassan an sms daily at 2 pm body take your medicine say it loudly",
-             "tell dad daily at 2 pm to take your medication now say it loudly",
-             "send a message to dad tells call me back asap",
-             "texting Samer Hassan every Friday at 5 pm tells it is a friendly reminder about our meeting today at 6 pm say it loudly",
-             "tell dad everyday this week at 7 am  to take your medication now notify me when it is answered",
-             "please send an sms daily this week at 2 pm to Hassan body take your medicine say it loudly"]
-"""
 maverickRecognizerGrammar = CFG.fromstring("""
-
 Command -> SimpleCommand | ComplexCommand | VariantCommand 
 
-SimpleCommand -> IntentPhrase ContactPhrase BodySentence 
-SimpleCommand -> CommandVerb Contacts Intent BodySentence 
-SimpleCommand -> CommandVerb Contacts BodySentence 
-SimpleCommand -> ContactPhrase IntentPhrase BodySentence 
+SimpleCommand -> IntentPhrase ContactPhrase BodySentence
+SimpleCommand -> CommandVerb ContactPhrase Intent BodySentence 
 
-
-IntentPhrase -> CommandVerb Intent | CommandVerb
-Intent -> "sms" | "an" "sms" | "message" | "a" "message"
-CommandVerb -> "send" | "text" | "inform" | "tell" | "texting" | "maverick" CommandVerb
-
+IntentPhrase -> CommandVerb Intent 
+Intent -> "an" "sms" | "a" "message"
+CommandVerb -> "send" | "text" | "inform" | "tell" | "texting" | "maverick" CommandVerb | "must" "be" "sent"
 ContactPhrase -> ContactPreposition Contacts
 ContactPreposition -> "to" | "for" | "into"
-Contacts -> "Shadi" | "Ahmad" | "Ali" | "Samer" "Hassan" | "Hassan" | "dad"
+Contacts -> TEXT
 
 BodySentence -> SMSInitial SMS 
-SMSInitial -> "says" | "that" "says" | "tells" | "body" |"content" | "to" | "that" |"telling"
+SMSInitial -> "that" "says" | "tells" | "body" |"content" | "telling" Determiner "that"
+Determiner -> "him" | "her"
 SMS -> TEXT 
 TEXT -> WORD | WORD TEXT | NUMBER | NUMBER TEXT
 
+# Complex Case 1: a command starts with an intent phrase ( i.e a verb + an intent) such as "send an sms"
 ComplexCommand -> IntentPhrase ContactPhrase TimePhrase BodySentence 
-ComplexCommand -> IntentPhrase Contacts TimePhrase BodySentence 
-#@samer accept intent after sms content 
-ComplexCommand -> IntentPhrase TimePhrase BodySentence ContactPhrase 
-ComplexCommand -> IntentPhrase TimePhrase ContactPhrase BodySentence 
-#@Samer 
-ComplexCommand -> IntentPhrase BodySentence ContactPhrase TimePhrase
-#@Samer 
-ComplexCommand -> IntentPhrase BodySentence TimePhrase ContactPhrase    
-ComplexCommand -> ContactPhrase IntentPhrase TimePhrase BodySentence
-ComplexCommand -> CommandVerb Contacts Intent TimePhrase BodySentence
-# @samer: accept to contacts 
+# Positions and variations for Case 1
+ComplexCommand -> IntentPhrase TimePhrase ContactPhrase BodySentence
+ComplexCommand -> IntentPhrase AdditionalCommand TimePhrase ContactPhrase BodySentence
+ComplexCommand -> IntentPhrase AdditionalCommand ContactPhrase TimePhrase BodySentence
+ComplexCommand -> IntentPhrase ContactPhrase AdditionalCommand TimePhrase BodySentence
+ComplexCommand -> IntentPhrase ContactPhrase AdditionalCommand BodySentence
+ComplexCommand -> IntentPhrase ContactPhrase TimePhrase AdditionalCommand BodySentence
+ComplexCommand -> IntentPhrase TimePhrase AdditionalCommand ContactPhrase BodySentence
+ComplexCommand -> IntentPhrase TimePhrase ContactPhrase AdditionalCommand BodySentence
+ComplexCommand -> IntentPhrase Time ContactPhrase RepeatPhrase BodySentence
+ComplexCommand -> IntentPhrase AdditionalCommand Time ContactPhrase RepeatPhrase BodySentence
+ComplexCommand -> IntentPhrase Time AdditionalCommand ContactPhrase RepeatPhrase BodySentence
+ComplexCommand -> IntentPhrase Time AdditionalCommand RepeatPhrase ContactPhrase BodySentence
+ComplexCommand -> IntentPhrase RepeatPhrase AdditionalCommand Time ContactPhrase BodySentence
+ComplexCommand -> IntentPhrase Time ContactPhrase AdditionalCommand RepeatPhrase BodySentence
+ComplexCommand -> IntentPhrase Time ContactPhrase RepeatPhrase AdditionalCommand BodySentence
+ComplexCommand -> IntentPhrase RepeatPhrase ContactPhrase Time BodySentence
+ComplexCommand -> IntentPhrase AdditionalCommand RepeatPhrase ContactPhrase Time BodySentence
+ComplexCommand -> IntentPhrase RepeatPhrase AdditionalCommand ContactPhrase Time BodySentence
+ComplexCommand -> IntentPhrase RepeatPhrase ContactPhrase AdditionalCommand Time BodySentence
+ComplexCommand -> IntentPhrase RepeatPhrase ContactPhrase Time AdditionalCommand BodySentence
+ComplexCommand -> IntentPhrase ContactPhrase Time AdditionalCommand RepeatPhrase BodySentence
+ComplexCommand -> IntentPhrase ContactPhrase RepeatPhrase AdditionalCommand Time BodySentence
+
+# Complex Case 2: a command starts with a verb such as "send" 
 ComplexCommand -> CommandVerb ContactPhrase Intent TimePhrase BodySentence
 
+# Positions and variations for Case 2
+ComplexCommand -> CommandVerb ContactPhrase Intent TimePhrase AdditionalCommand BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Intent Time AdditionalCommand RepeatPhrase BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Intent RepeatPhrase AdditionalCommand Time BodySentence
+ComplexCommand -> CommandVerb ContactPhrase TimePhrase Intent BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Time Intent RepeatPhrase BodySentence
+ComplexCommand -> CommandVerb ContactPhrase RepeatPhrase Intent Time BodySentence
+ComplexCommand -> CommandVerb TimePhrase Intent ContactPhrase BodySentence
+ComplexCommand -> CommandVerb TimePhrase ContactPhrase Intent BodySentence
+ComplexCommand -> CommandVerb Time Intent ContactPhrase RepeatPhrase BodySentence
+ComplexCommand -> CommandVerb Time Intent ContactPhrase RepeatPhrase AdditionalCommand BodySentence
+ComplexCommand -> CommandVerb RepeatPhrase Intent ContactPhrase Time BodySentence
+ComplexCommand -> CommandVerb RepeatPhrase Intent ContactPhrase Time AdditionalCommand BodySentence
+ComplexCommand -> CommandVerb Time ContactPhrase Intent RepeatPhrase BodySentence
+ComplexCommand -> CommandVerb Time ContactPhrase RepeatPhrase Intent BodySentence
+ComplexCommand -> CommandVerb RepeatPhrase ContactPhrase Intent Time BodySentence
+ComplexCommand -> CommandVerb RepeatPhrase ContactPhrase Time Intent BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Intent AdditionalCommand BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Intent AdditionalCommand BodySentence
+ComplexCommand -> CommandVerb ContactPhrase Intent AdditionalCommand TimePhrase BodySentence
+ComplexCommand -> CommandVerb ContactPhrase TimePhrase Intent AdditionalCommand BodySentence
 
 VariantCommand -> PoliteExpression SimpleCommand | PoliteExpression ComplexCommand
-VariantCommand -> SimpleCommand AdditionalCommand | ComplexCommand AdditionalCommand
-VariantCommand -> PoliteExpression SimpleCommand AdditionalCommand | PoliteExpression ComplexCommand AdditionalCommand
+
+TimePhrase -> RepeatPhrase Time | Time RepeatPhrase | Time
+RepeatPhrase -> "repeat" TEXT 
+Time -> TimePreposition TEXT
+TimePreposition -> "at" | "on"
 
 
-TimePhrase -> RepeatPhrase TimePreposition Time 
-RepeatPhrase -> Repeat TimeDeterminer Range
-Repeat -> "repeat" |
-TimeDeterminer -> "daily" | "everyday" | "every" Day | 
-Day -> "Friday" | "Saturday" | "Sunday" | "Monday" |
-Range -> "this" "week" | "this" "month" |
-TimePreposition -> "at" 
-Time -> TEXT
-
-
-PoliteExpression -> "please" | "would" "you" "please" | "could" "you" | "I" "would" "like"
+PoliteExpression -> "please" | "would" "you" "please" | "could" "you" | "I" "would" "like" | "I" "wish" "to" 
 
 AdditionalCommand -> AdditionalCommandInitial AdditionalCommandWhat AdditionalCommandHow
-AdditionalCommandInitial -> "say" | "deliver" | "read" | "notify" "me" "when"
-AdditionalCommandWhat -> "it" | "the" "content" | "the" "message" | "the" "body" 
+AdditionalCommandInitial -> "say" 
+AdditionalCommandWhat -> "it" | "the" "content" | "the" "message" | "the" "body" | "this" "message"
 AdditionalCommandHow ->   "loudly" | "quietly" | "softly" | "aloud" | "is" "answered" | "is" "delivered"
 """)
 
@@ -127,7 +134,41 @@ def parseToList(s):
  i=0
  for tree in results:
     i+=1
-    """print(tree)"""
+    print(tree)
+    print("**********************Structured Model***********************")
+    for subtree in tree.subtrees():
+        if subtree.label() == 'Intent':
+            print("Intent = ", subtree.leaves())
+        elif subtree.label() == 'CommandVerb':
+            print("CommandVerb = ", subtree.leaves())
+        elif subtree.label() == 'Contacts':
+            print("Contacts = ", subtree.leaves())
+        elif subtree.label() == 'ContactPreposition':
+            print("ContactPreposition = ", subtree.leaves())
+        elif subtree.label() == 'RepeatPhrase':
+            print("RepeatPhrase = ", subtree.leaves())
+        elif subtree.label() == 'TimePreposition':
+            print("TimePreposition = ", subtree.leaves())
+        elif subtree.label() == 'Time':
+            print("Time = ", subtree.leaves())
+        elif subtree.label() == 'SMSInitial':
+            print("SMSInitial = ", subtree.leaves())
+        elif subtree.label() == 'SMS':
+            print("SMS = ", subtree.leaves())
+        elif subtree.label() == 'AdditionalCommand':
+            print("AdditionalCommand = ", subtree.leaves())
+
+    print("=============================================")
+
+ if (i==0):
+   print("====================Not parsed=========================")
+ if (i<=1):
+   print("=============================================")
+ else:
+   print("=====================Ambiguity========================")
+
+ return (i>0 and i<2)
+ """
     print("=============================================")
     for subtree in tree.subtrees():
         if subtree.label() == 'Intent':
@@ -138,8 +179,8 @@ def parseToList(s):
             print("Contacts = ", subtree.leaves())
         elif subtree.label() == 'ContactPreposition':
             print("ContactPreposition = ", subtree.leaves())
-        elif subtree.label() == 'TimeDeterminer':
-            print("TimeDeterminer = ", subtree.leaves())
+        elif subtree.label() == 'RepeatPhrase':
+            print("RepeatPhrase = ", subtree.leaves())
         elif subtree.label() == 'TimePreposition':
             print("TimePreposition = ", subtree.leaves())
         elif subtree.label() == 'Time':
@@ -151,16 +192,7 @@ def parseToList(s):
         elif subtree.label() == 'AdditionalCommand':
             print("AdditionalCommand = ", subtree.leaves())
     print("=============================================")
-
- if (i==0):
-   print("====================Not parsed=========================")
- if (i<=1):
-   print("=============================================")
- else:
-   print("=====================Ambiguity========================")
-
- return (i>0 and i<2)
-
+ """
 def literal_production(key, rhs):
     """ Return a production <key> -> n
 
@@ -180,16 +212,14 @@ def parse_maverick_command(command):
     # extract new words and numbers
     words = set([match.group(0) for match in re.finditer(r"[a-zA-Z]+", command)])
     numbers = set([match.group(0) for match in re.finditer(r"\d+", command)])
-    finalwords = []
-    for word in words:
-        if not ( word == "say" or word == "notify"):
-            finalwords += [word]
+
     # Make a local copy of productions
     local_maverick_productions = list(maverickRecognizerProductions)
 
     # Add a production for every words and number
-    local_maverick_productions.extend([literal_production("WORD", word) for word in finalwords])
+    local_maverick_productions.extend([literal_production("WORD", word) for word in words])
     local_maverick_productions.extend([literal_production("NUMBER", number) for number in numbers])
+
 
     # Make a local copy of the grammar with extra productions
     local_maverick_grammar = CFG(maverickRecognizerGrammar.start(), local_maverick_productions)
@@ -213,6 +243,10 @@ print(true/len(sentences))
 
 
 # run the file on different cases
+
+
+
+
 
 
 
