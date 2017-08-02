35ce2bef499ae393fe0d28f87208e2d7fc8efbd6
hassan Loulou <hassan.loulou@u-psud.fr>
2017-08-02 17:20:38 +0200

    Voice command problems solved!

--- ParseWithAPP.py
+++ ParseWithAPP.py
@@ -315,6 +315,55 @@ def stt2():
     except sr.RequestError as e:
         ment.set("Could not request results from Google Speech Recognition service; {0}")
 
+
+def stt3():
+    # enter the name of usb microphone that you found
+    # using lsusb
+    # the following name is only used as an example
+    mic_name = "USB Device 0x46d:0x825: Audio (hw:1, 0)"
+    # Sample rate is how often values are recorded
+    sample_rate = 48000
+    # Chunk is like a buffer. It stores 2048 samples (bytes of data)
+    # here.
+    # it is advisable to use powers of 2 such as 1024 or 2048
+    chunk_size = 2048
+    # Initialize the recognizer
+    r = sr.Recognizer()
+
+    # generate a list of all audio cards/microphones
+    mic_list = sr.Microphone.list_microphone_names()
+
+    # the following loop aims to set the device ID of the mic that
+    # we specifically want to use to avoid ambiguity.
+    for i, microphone_name in enumerate(mic_list):
+        if microphone_name == mic_name:
+            device_id = i
+
+    # use the microphone as source for input. Here, we also specify
+    # which device ID to specifically look for incase the microphone
+    # is not working, an error will pop up saying "device_id undefined"
+    with sr.Microphone(device_index=1, sample_rate=sample_rate,
+                       chunk_size=chunk_size) as source:
+        # wait for a second to let the recognizer adjust the
+        # energy threshold based on the surrounding noise level
+        r.adjust_for_ambient_noise(source)
+        print("Say Something")
+        # listens for the user's input
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            ment.set(text)
+
+        # error occurs when google could not understand what was said
+
+        except sr.UnknownValueError:
+            ment.set("Google Speech Recognition could not understand audio")
+
+        except sr.RequestError as e:
+            ment.set("Could not request results from Google Speech{0}".format(e))
+
+
 def yousaid():
     tts = gTTS(text=ment.get(), lang='en', slow=False)
     tts.save("hh2.mp3")
@@ -349,7 +398,7 @@ def GenGui():
 
     mEntry=Entry(mGui,textvariable=ment, width= 120).pack()
 
-    mButtonv = Button(mGui, text="Enter a Voice Command!", command=stt2).pack()
+    mButtonv = Button(mGui, text="Enter a Voice Command!", command=stt3).pack()
 
     #mEntry2=Entry(mGui,textvariable =ment5, width= 120).pack()
 
