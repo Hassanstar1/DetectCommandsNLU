df6f8e32ea619694995ea3adfdf9d3156d5d8c8d
ALISem <baddour.ali85@gmail.com>
2017-08-30 18:21:23 +0800

    Build a new CFG that can accept other variations of commands; sentences that begin with the time slot, the contact slot or an initial/polite talk.

--- 21.py
+++ 21.py
@@ -6,41 +6,51 @@ from nltk.draw import tree
 
 #sentences=["send an sms to dad content good morning take your medicine"]tell Ali the following message xxxx", "text Ali with the following sms xxx", "send an sms that contains the content xxx to Ali"]
 #sentences=["send an sms to dad at 9 am July 13 2017 repeat every 4 hours   say it loudly content good morning Dad take your medicine"]
-sentences =["send a message to dad",
+sentences =["please send an sms to dad at 9 am repeat everyday  say it loudly  content good morning",
+            "to dad send a message content take your medication dad my dad",
+            "at 10 pm send a message tells i would like to meet you at your office tommorow morning at 8 pm",
+            "i would like sending a message to dad content take your medication dad",
+            "leave a message content take your medication dad",
+            "give a message to dad content take your medication dad",
+            "i want to deliver a message for dad content take your medication dad",
+            "to dad at 10 pm send a message tells take your medication",
+            "to dad send a message at 10 pm  tells call me back",
+            "send a message say it in loud voice at 10 pm",
             "please I would like to send a message to Samer Hassan",
             "Could you please send a message that says i would like to meet you at your office tommorow morning at 8 pm",
-            "send to dad                                    content take your medication",
-            "send an sms to dad                                         content take your medication",
-            "send an sms to dad                           say it loudly content take your medication",
-            "send an sms to dad at 9 am                   say it loudly content take your medication",
-            "send an sms to dad at 9 am  repeat everyday  say it loudly content take your medication",
-            "send an sms at 9 am content good morning to my dad your office",
-            "send an sms to dad at 9 am repeat everyday   say it loudly content good morning take your medicine"]
+
+            "please send to dad                                    content take your medication",
+            "please send an sms to dad                                         content take your medication",
+            "please send an sms to dad                           say it loudly content take your medication",
+            "please send an sms to dad at 9 am                   say it loudly content take your medication",
+            "please send an sms to dad at 9 am  repeat everyday  say it loudly content take your medication",
+            "please send an sms at 9 am content good morning",
+            "please send an sms to dad at 9 am repeat everyday   say it loudly content good morning dad take your medicine"]
 """
-            "send an sms to dad at 9 am repeat everyday    say it loudly content good morning ",
-            "send to dad an sms at 9 am repeat everyday   say it loudly content good morning ",
-            "send to dad at 9 am repeat everyday  an sms   say it loudly content good morning ",
-            "send to dad an sms at 9 am repeat everyday    say it loudly content good morning",
-            "send an sms at 9 am to dad repeat everyday    say it loudly content good morning ",
-            "send an sms at 9 am repeat everyday  to dad   say it loudly content good morning",
-            "send an sms at 9 am repeat everyday   to dad say it loudly content good morning",
-            "send at 9 am an sms to dad repeat everyday    say it loudly content good morning",
-            "send an sms at 9 am to dad repeat everyday    say it loudly content good morning",
-            "send an sms to dad repeat everyday  at 9 am   say it loudly content good morning",
-            "send an sms to dad repeat everyday   at 9 am  say it loudly content good morning",
-            "send an sms to dad repeat everyday     say it loudly at 9 am content good morning",
-            "send an sms repeat everyday to dad at 9 am    say it loudly content good morning",
-            "send an sms to dad repeat everyday  at 9 am    say it loudly content good morning",
-            "send an sms to dad at 9 am    repeat everyday  say it loudly content good morning",
-            "send an sms to dad at 9 am    say it loudly repeat everyday  content good morning",
-            "send an sms   to dad at 9 am repeat everyday  say it loudly content good morning",
-            "send an sms to dad   at 9 am repeat everyday  say it loudly content good morning",
-            "send an sms to dad at 9 am   repeat everyday  say it loudly content good morning",
-            "send an sms to dad at 9 am repeat everyday  say it loudly  content good morning",
-            "send an sms say it loudly to dad at 9 am repeat everyday  content good morning",
-            "send an sms to dad say it loudly at 9 am repeat everyday   content good morning",
-            "send an sms to dad at 9 am say it loudly repeat everyday   content good morning",
-            "send an sms to dad at 9 am repeat everyday  say it loudly  content good morning"]
+            "please send an sms to dad at 9 am repeat everyday    say it loudly content good morning ",
+            "please send to dad an sms at 9 am repeat everyday   say it loudly content good morning ",
+            "please send to dad at 9 am repeat everyday  an sms   say it loudly content good morning ",
+            "please send to dad an sms at 9 am repeat everyday    say it loudly content good morning",
+            "please send an sms at 9 am to dad repeat everyday    say it loudly content good morning ",
+            "please send an sms at 9 am repeat everyday  to dad   say it loudly content good morning",
+            "please send an sms at 9 am repeat everyday   to dad say it loudly content good morning",
+            "please send at 9 am an sms to dad repeat everyday    say it loudly content good morning",
+            "please send an sms at 9 am to dad repeat everyday    say it loudly content good morning",
+            "please send an sms to dad repeat everyday  at 9 am   say it loudly content good morning",
+            "please send an sms to dad repeat everyday   at 9 am  say it loudly content good morning",
+            "please send an sms to dad repeat everyday     say it loudly at 9 am content good morning",
+            "please send an sms repeat everyday to dad at 9 am    say it loudly content good morning",
+            "please send an sms to dad repeat everyday  at 9 am    say it loudly content good morning",
+            "please send an sms to dad at 9 am    repeat everyday  say it loudly content good morning",
+            "please send an sms to dad at 9 am    say it loudly repeat everyday  content good morning",
+            "please send an sms   to dad at 9 am repeat everyday  say it loudly content good morning",
+            "please send an sms to dad   at 9 am repeat everyday  say it loudly content good morning",
+            "please send an sms to dad at 9 am   repeat everyday  say it loudly content good morning",
+            "please send an sms to dad at 9 am repeat everyday  say it loudly  content good morning",
+            "please send an sms say it loudly to dad at 9 am repeat everyday  content good morning",
+            "please send an sms to dad say it loudly at 9 am repeat everyday   content good morning",
+            "please send an sms to dad at 9 am say it loudly repeat everyday   content good morning",
+            "please send an sms to dad at 9 am repeat everyday  say it loudly  content good morning"]
 """
 maverickRecognizerGrammar6 = CFG.fromstring("""
 
@@ -102,15 +112,40 @@ SMS -> TEXT
 TEXT -> WORD | WORD TEXT | NUMBER | NUMBER TEXT
 
 """)
-
+#  Build a new CFG that can accept other variations of commands; sentences begin with the time slot, the contact slot or an initial/polite talk.
 maverickRecognizerGrammar1 = CFG.fromstring("""
-Command -> TEXT0 CommandSlots | CommandSlots
-CommandSlots -> Slot | Slot CommandSlots
-Slot -> Intent | CommandVerb | TimeSlot | AdditionalSlot | ContentSlot | ContactSlot | FrequencySlot
+Command -> CommandSlots ContentSlot | TEXT0 CommandSlots ContentSlot
+CommandSlots -> Slot | Slot CommandSlots 
+Slot -> Intent | CommandVerb | TimeSlot | AdditionalSlot | ContactSlot | FrequencySlot
 Intent ->  "an" "sms" | "a" "message" 
 CommandVerb -> "send" | "text" | "inform" | "tell" | "texting" | "maverick" CommandVerb | "must" "be" "sent" | "sending"
-ContentSlot -> "that" "says" Content | "tells" Content | "body" Content |"content" Content
-Content -> TEXT1
+ContentSlot -> "that" "says" Content | "tells" Content | "body" Content | "content" Content |
+Content -> TEXT1 
+ContactSlot -> "to" Contact | "for" Contact | "into" Contact
+Contact -> TEXT2
+TimeSlot ->  "at" Time
+Time -> TEXT3
+AdditionalSlot -> "say" Additional| "notify" Additional
+Additional ->  TEXT4
+FrequencySlot -> "repeat" Frequency
+Frequency -> TEXT5 
+TEXT0 -> WORD0 | WORD0 TEXT0 | NUMBER0 | NUMBER0 TEXT0 
+TEXT1 -> TEXT 
+TEXT ->  WORD | WORD TEXT | NUMBER | NUMBER TEXT  
+TEXT2 -> WORD2 | WORD2 TEXT2 | NUMBER2 | NUMBER2 TEXT2
+TEXT3 -> WORD3 | WORD3 TEXT3 | NUMBER3 | NUMBER3 TEXT3
+TEXT4 -> WORD4 | WORD4 TEXT4 | NUMBER4 | NUMBER4 TEXT4
+TEXT5 -> WORD5 | WORD5 TEXT5 | NUMBER5 | NUMBER5 TEXT5
+""")
+
+maverickRecognizerGrammar12 = CFG.fromstring("""
+Command -> CommandSlots | TEXT0 CommandSlots
+CommandSlots -> Slot | Slot CommandSlots 
+Slot -> Intent | CommandVerb | TimeSlot | AdditionalSlot | ContentSlot | ContactSlot | FrequencySlot
+Intent ->  "an" "sms" | "a" "message"  
+CommandVerb -> "send" | "text" | "inform" | "tell" | "texting" | "maverick" CommandVerb | "must" "be" "sent" | "sending"
+ContentSlot -> "that" "says" Content | "tells" Content | "body" Content | "content" Content 
+Content -> TEXT6
 ContactSlot -> "to" Contact | "for" Contact | "into" Contact
 Contact -> TEXT2
 TimeSlot ->  "at" Time
@@ -127,7 +162,10 @@ TEXT2 -> WORD2 | WORD2 TEXT2 | NUMBER2 | NUMBER2 TEXT2
 TEXT3 -> WORD3 | WORD3 TEXT3 | NUMBER3 | NUMBER3 TEXT3
 TEXT4 -> WORD4 | WORD4 TEXT4 | NUMBER4 | NUMBER4 TEXT4
 TEXT5 -> WORD5 | WORD5 TEXT5 | NUMBER5 | NUMBER5 TEXT5
+TEXT6 -> WORD6 | WORD6 TEXT6 | NUMBER6 | NUMBER5 TEXT6
 """)
+
+
 
 def parseToList(s):
  results = None
@@ -157,7 +195,7 @@ def isResultsNotNull(results):
     #print(tree)
     if (i == 1):
         PrintResult(tree)
-        tree.draw()
+        #tree.draw()
         if not receiverIsSpecified(tree):
           print("The Receiver is missing in this command")
     return (i>0 and i<2)
@@ -195,7 +233,18 @@ def PrintResult(tree):
         elif subtree.label() == 'TEXT1':
             print("", subtree.leaves())
         elif subtree.label() == 'Contact':
-            print("", subtree.leaves())
+            print("Receiver = ", subtree.leaves())
+        elif subtree.label() == 'Intent':
+            print("Intent =", subtree.leaves())
+        elif subtree.label() == 'Content':
+            print("Content = ", subtree.leaves())
+        elif subtree.label() == 'Time':
+            print("Time = ", subtree.leaves())
+        elif subtree.label() == 'AdditionalSlot':
+            print("Additional = ", subtree.leaves())
+        elif subtree.label() == 'FrequencySlot':
+            print("Frequency = ", subtree.leaves())
+
 
 def PrintResult2(tree):
 
@@ -210,7 +259,7 @@ def PrintResult2(tree):
 
         if subtree.label() == 'Keyword':
             if subtree.leaves()[0] == 'se':
-               print("Intent", subtree.leaves())
+               print("Intent =", subtree.leaves())
             elif subtree.leaves()[0] == 'to':
                 print("Receiver", subtree.leaves())
             elif subtree.leaves()[0] == 'at':
@@ -249,7 +298,11 @@ def parse_maverick_command(command,i):
     words = set([match.group(0) for match in re.finditer(r'''(['()""\w.]+|\.+|\?+|\,+|\!+|\:+|\;)''', command)])
     #words = set([match.group(0) for match in re.finditer(r"[a-zA-Z.]+|\.+|\?+|\,+|\!+|\:+|\;", command)])
     numbers = set([match.group(0) for match in re.finditer("[-+]?\d+[\.]?\d*", command)])
-    kewords = ["send","sending"]
+    InitialExclusion = ["send","sending", "a", "message", "content", "tells"]
+    ContactExclusion = ["send", "sending", "content","a", "message", "tells", "to", "for", "repeat", "say", "notify", "at"]
+    AdditionalExclusion = ["at","repeat","content", "tells", "to", "for","at"]
+    FrequencyExclusion = ["say","notify","content", "tells", "to","for", "at"]
+    TimeExclusion = ["say", "notify", "content", "tells", "to", "for", "repeat"]
     # Make a local copy of productions
 
     if (i == 6):
@@ -267,15 +320,19 @@ def parse_maverick_command(command,i):
 
     # Add a production for every words and number
     local_maverick_productions.extend([literal_production("WORD", word) for word in words])
-    local_maverick_productions.extend([literal_production("NUMBER", number) for number in numbers])
-    local_maverick_productions.extend([literal_production("WORD0", word) for word in words if word not in kewords])
+    #local_maverick_productions.extend([literal_production("NUMBER", number) for number in numbers])
+    local_maverick_productions.extend([literal_production("WORD0", word) for word in words if word not in InitialExclusion])
     local_maverick_productions.extend([literal_production("NUMBER0", number) for number in numbers])
-    local_maverick_productions.extend([literal_production("WORD2", word) for word in words if word not in kewords])
+    local_maverick_productions.extend([literal_production("WORD2", word) for word in words if word not in ContactExclusion])
     local_maverick_productions.extend([literal_production("NUMBER2", number) for number in numbers])
-    local_maverick_productions.extend([literal_production("WORD3", word) for word in words])
+    local_maverick_productions.extend([literal_production("WORD3", word) for word in words if word not in TimeExclusion])
     local_maverick_productions.extend([literal_production("NUMBER3", number) for number in numbers])
-    local_maverick_productions.extend([literal_production("WORD4", word) for word in words])
+    local_maverick_productions.extend([literal_production("WORD4", word) for word in words if word not in AdditionalExclusion])
     local_maverick_productions.extend([literal_production("NUMBER4", number) for number in numbers])
+    local_maverick_productions.extend([literal_production("WORD5", word) for word in words if word not in FrequencyExclusion])
+    local_maverick_productions.extend([literal_production("NUMBER5", number) for number in numbers])
+    local_maverick_productions.extend([literal_production("WORD6", word) for word in words])
+    local_maverick_productions.extend([literal_production("NUMBER6", number) for number in numbers])
 
     # Make a local copy of the grammar with extra productions
     if (i==6):
